46db8e7f7cdcc3426a1e47af671e8258623adbce
BrokenFridge <103411852+Arq091@users.noreply.github.com>
2022-09-04 18:26:08 +0700

    changed alignment

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,9 @@ import logo from "/logo.svg";
 
 function Nav() {
   return (
-    <nav className="flex text-slate-800 sm:bg-gray-400/70 bg-gray-300 sm:backdrop-blur-sm z-40 px-[2rem] items-center justify-between border-b border-gray-400 sticky top-0">
+    <nav className="flex text-slate-800 sm:bg-gray-400/70 bg-gray-300 sm:backdrop-blur-sm z-40 py-4 px-[2rem] items-center justify-center sm:justify-between border-b border-gray-400 sticky top-0">
       <Link className="font-bold text-[1.75rem] flex items-center" to="/">
-        <img src={logo} alt="" width="60" />
+        <img src={logo} alt="" width="50" className="mt-[0.2rem]" />
         <span className="text-black text-[2rem]">ayso</span>
       </Link>
       <section className="space-x-5 text-[1.1rem] absolute hidden opacity-0 sm:block sm:relative sm:opacity-100">
